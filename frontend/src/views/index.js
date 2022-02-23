e2a5281c5818f7ad3c380e84f0e80472e418e991
AbdullahIsha <shahariaresha@gmail.com>
2022-02-24 00:57:00 +0600

    new template place added

--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -1,56 +1,13 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom';
-import "./../assets/css/main.css";
-import MainNav from './../Component/nav.js';
-import { Row, Col, Container } from 'react-bootstrap';
+//import "./../assets/css/main.css";
+// import MainNav from './../Component/nav.js';
+// import { Row, Col, Container } from 'react-bootstrap';
 
 const Index = () => {
     const history = useHistory();
     return (
         <>
-        <MainNav/>
-        <Container>
-          {/* Stack the columns on mobile by making one full-width and the other half-width */}
-          <Row>
-            <Col xs={12} md={3}>
-            <h2>Menu List.</h2>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={4}>
-            <h2>Menu List.</h2>
-            </Col>
-            <Col xs={12} md={4}>
-            <h2>Menu List.</h2>
-            </Col>
-            <Col xs={12} md={4}>
-            <h2>Menu List.</h2>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={4}>
-            <h2>Menu List.</h2>
-            </Col>
-            <Col xs={12} md={4}>
-            <h2>Menu List.</h2>
-            </Col>
-            <Col xs={12} md={4}>
-            <h2>Menu List.</h2>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={4}>
-            <h2>Menu List.</h2>
-            </Col>
-            <Col xs={12} md={4}>
-            <h2>Menu List.</h2>
-            </Col>
-            <Col xs={12} md={4}>
-            <h2>Menu List.</h2>
-            </Col>
-          </Row>
-        </Container>
-        
         <button type="button" class="btn btn-outline-secondary" onClick={() => history.push('/useState')}>Use State</button>
         </>
     )
