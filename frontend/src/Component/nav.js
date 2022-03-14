2893d130dd2f7476382ee76953ddd641ab46a2ed
AbdullahIsha <shahariaresha@gmail.com>
2022-03-15 00:26:40 +0600

    tested navber

--- frontend/src/Component/nav.js
+++ frontend/src/Component/nav.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import './../assets/css/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Nav,  Navbar} from 'react-bootstrap';
+import React from "react";
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 
 const MainNav = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand href="#home" style={{paddingLeft: "20px"}}>React-Bootstrap</Navbar.Brand>
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="navbar-nav ms-auto mb-2 mb-lg-0" style={{paddingRight: "20px"}}>
-                  <Nav.Link href="#features">Features</Nav.Link>
-                  <Nav.Link href="#pricing">Pricing</Nav.Link>
-                  <Nav.Link href="#deets">More details</Nav.Link>
-                  <Nav.Link eventKey={2} href="#memes">
-                      Good stuff
-                  </Nav.Link>
-              </Nav>
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
           </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   );
 };
-  
-export default MainNav
+
+export default MainNav;
--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom';
-//import "./../assets/css/main.css";
-// import MainNav from './../Component/nav.js';
-// import { Row, Col, Container } from 'react-bootstrap';
 
 const Index = () => {
     const history = useHistory();
