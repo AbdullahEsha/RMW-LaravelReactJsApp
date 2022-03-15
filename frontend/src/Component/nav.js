e4c9a8198f308eaa57d2ffdf195e712ec84acb32
AbdullahIsha <shahariaresha@gmail.com>
2022-03-15 16:30:46 +0600

    navber, background img and card added

--- frontend/src/Component/nav.js
+++ frontend/src/Component/nav.js
@@ -1,36 +1,19 @@
 import React from "react";
-import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 const MainNav = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/menu">Menu</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/bookTable">Book Table</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -1,13 +1,111 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import MainNav from "./../Component/nav.js";
+import "./../Component/nav.js";
+import "../assets/css/main.css";
+import ImgMid1 from "../assets/images/o1.jpg";
+import ImgMid2 from "../assets/images/o2.jpg";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Index = () => {
-    const history = useHistory();
-    return (
-        <>
-        <button type="button" class="btn btn-outline-secondary" onClick={() => history.push('/useState')}>Use State</button>
-        </>
-    )
-}
+  const history = useHistory();
+  return (
+    <>
+      <MainNav />
+      <Container>
+        <Row>
+          <Col xs={12} md={6}>
+            <div>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <i>
+                <h1>Lorem Ipsum is simply lorem including of Lorem Ipsum.</h1>
+              </i>
+              <i>
+                <h6>
+                  Lorem Ipsum is simply dummy text of the Aldus PageMaker
+                  including versions of Lorem Ipsum.Lorem Ipsum is simply dummy
+                  text of the Aldus PageMaker including versions of Lorem Ipsum.
+                </h6>
+              </i>
+            </div>
+            <br />
+            <button
+              type="button"
+              class="btn btn-outline-secondary"
+              onClick={() => history.push("/useState")}
+            >
+              Use State
+            </button>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+          </Col>
+          <Col xs={0} md={6}></Col>
+        </Row>
+      </Container>
 
-export default Index
+      <Container>
+        <Row>
+          <Col xs={12} md={6} align="center">
+            <div className="card" id="crd">
+              <Row>
+                <Col xs={6} md={6}>
+                  <img src={ImgMid1} className="card-img-top" alt="ImgMid1" />
+                </Col>
+                <Col xs={6} md={6}>
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col xs={12} md={6} align="center">
+            <div className="card" id="crd">
+              <Row>
+                <Col xs={6} md={6}>
+                  <img src={ImgMid2} className="card-img-top" alt="ImgMid2" />
+                </Col>
+                <Col xs={6} md={6}>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <div align="center">
+          <h1>Menu</h1>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Index;
