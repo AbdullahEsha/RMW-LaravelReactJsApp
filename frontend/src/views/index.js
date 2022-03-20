f96b31697859845438bd8d5898e99495684e4ec9
AbdullahIsha <shahariaresha@gmail.com>
2022-03-20 22:49:10 +0600

    menucard image loaded

--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -7,6 +7,7 @@ import MenuCard from "./../Component/menuCard";
 import "../assets/css/style.css";
 import ImgMid1 from "../assets/images/o1.jpg";
 import ImgMid2 from "../assets/images/o2.jpg";
+import ImgBnr from "../assets/images/aboutImg.png";
 import { Container, Row, Col } from "react-bootstrap";
 
 const uniqueList = [
@@ -54,10 +55,12 @@ const Index = () => {
               <br />
               <br />
               <i>
-                <h1>Lorem Ipsum is simply lorem including of Lorem Ipsum.</h1>
+                <h1 style={{ color: "white" }}>
+                  Lorem Ipsum is simply lorem including of Lorem Ipsum.
+                </h1>
               </i>
               <i>
-                <h6>
+                <h6 style={{ color: "white" }}>
                   Lorem Ipsum is simply dummy text of the Aldus PageMaker
                   including versions of Lorem Ipsum.Lorem Ipsum is simply dummy
                   text of the Aldus PageMaker including versions of Lorem Ipsum.
@@ -86,56 +89,94 @@ const Index = () => {
           <Col xs={0} md={6}></Col>
         </Row>
       </Container>
+      <section className="menuBarCard">
+        <br />
+        <br />
+        <Container>
+          <Row>
+            <Col xs={12} md={6} align="center" className="clsbtm">
+              <div className="cardx" id="crdx">
+                <Row>
+                  <Col xs={6} md={6}>
+                    <img src={ImgMid1} className="card-img-top" alt="ImgMid1" />
+                  </Col>
+                  <Col xs={6} md={6}>
+                    <div className="card-body">
+                      <h5 className="card-title" style={{ color: "white" }}>
+                        Card title
+                      </h5>
+                      <p className="card-text" style={{ color: "white" }}>
+                        Some quick example text to build onthe card's content.
+                      </p>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+            <Col xs={12} md={6} align="center" className="clsbtm">
+              <div className="cardx" id="crdx">
+                <Row>
+                  <Col xs={6} md={6}>
+                    <img src={ImgMid2} className="card-img-top" alt="ImgMid2" />
+                  </Col>
+                  <Col xs={6} md={6}>
+                    <div class="card-body">
+                      <h5 class="card-title" style={{ color: "white" }}>
+                        Card title
+                      </h5>
+                      <p class="card-text" style={{ color: "white" }}>
+                        Some quick example text to build bulk of the card's
+                        content.
+                      </p>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </Container>
 
-      <Container>
-        <Row>
-          <Col xs={12} md={6} align="center" className="clsbtm">
-            <div className="card" id="crd">
-              <Row>
-                <Col xs={6} md={6}>
-                  <img src={ImgMid1} className="card-img-top" alt="ImgMid1" />
-                </Col>
-                <Col xs={6} md={6}>
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                  </div>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-          <Col xs={12} md={6} align="center" className="clsbtm">
-            <div className="card" id="crd">
-              <Row>
-                <Col xs={6} md={6}>
-                  <img src={ImgMid2} className="card-img-top" alt="ImgMid2" />
-                </Col>
-                <Col xs={6} md={6}>
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                  </div>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
         <div align="center">
           <h1>Menu</h1>
         </div>
-      </Container>
-      <Container>
-        <Menubar filterItem={filterItem} menuList={menuList} />
-        <MenuCard menuData={menuData} />
-      </Container>
+        <Container>
+          <Menubar filterItem={filterItem} menuList={menuList} />
+          <MenuCard menuData={menuData} />
+        </Container>
+        <br />
+        <br />
+      </section>
+      <section className="drksec">
+        <Row>
+          <Col xs={12} md={6}>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <div align="center">
+              <h1 style={{ color: "white" }}>Some quick example</h1>
+              <h5 style={{ color: "white" }}>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </h5>
+            </div>
+            <br />
+            <br />
+          </Col>
+          <Col xs={12} md={6}>
+            <div align="center">
+              <br />
+              <img src={ImgBnr} className="" alt="ImgBnr" height="350px" />
+              <br />
+              <br />
+            </div>
+          </Col>
+        </Row>
+      </section>
     </>
   );
 };
