efae304bfdda5e74ee1d27772b15ff4fc137136e
AbdullahIsha <shahariaresha@gmail.com>
2022-03-21 00:41:25 +0600

    menu card design changed

--- frontend/src/Component/menuCard.js
+++ frontend/src/Component/menuCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 const MenuCard = ({ menuData }) => {
   console.log(menuData);
@@ -6,23 +7,37 @@ const MenuCard = ({ menuData }) => {
     <>
       <section className="main-card--cointainer">
         {menuData.map((curElem) => {
-          const { id, name, category, image, description } = curElem;
+          const { id, name, category, image, price, description } = curElem;
 
           return (
             <>
               <div className="card-container" key={id}>
-                <div className="card" style={{ backgroundColor: "#222831" }}>
-                  <div className="card-body">
-                    <span className="card-number card-circle subtle">{id}</span>
-                    <span className="card-author subtle"> {category}</span>
+                <div className="card">
+                  {/* <span className="card-number card-circle subtle">{id}</span>
+                  <span className="card-author subtle"> {category}</span> */}
+                  <img src={image} alt="images" className="card-media" />
+                  <div
+                    className="card-body"
+                    style={{ backgroundColor: "#222831" }}
+                  >
                     <h2 className="card-title"> {name} </h2>
+                    <h6 className="card-title">{category}</h6>
                     <span className="card-description subtle">
                       {description}
                     </span>
-                    <div className="card-read">Read</div>
+                    <br />
+                    <br />
+                    <Row>
+                      <Col xs={6} md={6}>
+                        <h3 style={{ color: "#fff" }}>{price}</h3>
+                      </Col>
+                      <Col xs={6} md={6} align="right">
+                        <a href="/order" id="odr">
+                          Order Now
+                        </a>
+                      </Col>
+                    </Row>
                   </div>
-                  <img src={image} alt="images" className="card-media" />
-                  <span className="card-tag  subtle">Order Now</span>
                 </div>
               </div>
             </>
