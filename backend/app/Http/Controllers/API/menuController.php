d32caf1c514cf4ea8def0f83b9fc7851db97339e
AbdullahIsha <shahariaresha@gmail.com>
2022-03-27 02:27:20 +0600

    upload menu done

--- backend/app/Http/Controllers/API/menuController.php
+++ backend/app/Http/Controllers/API/menuController.php
@@ -11,18 +11,17 @@ class menuController extends Controller
     public function store(Request $req){
         $menu = new Menu;
         
-        $rename_to = input(file('img')->getClientOriginalName());
-        $menu->name = $req->input('name');
-        $menu->category = $req->input('category');
-        $menu->price = $req->input('price');
-        $menu->description = $req->input('description');
-        $menu->img = $req->$rename_to;
+        
+        $menu->name = $req->name;
+        $menu->category = $req->category;
+        $menu->price = $req->price;
+        $menu->description = $req->description;
+        $menu->img = $req->img;
 
         $menu->save();
 
         return  response()-> json([
-            "status" => 200,
-            "mes" => 'item added to the menu table',
+            "message" => 'Item added to the menu table...',
         ]);
     }
 }
--- frontend/src/views/admin/uploadItem.js
+++ frontend/src/views/admin/uploadItem.js
@@ -1,11 +1,41 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Form } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import "../../assets/css/style.css";
 import Footer from "../../Component/footer";
 import MainNav from "../../Component/nav";
-// import axios from "axios";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 const FullMenu = () => {
+  const history = useHistory();
+
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState();
+  const [description, setDescription] = useState();
+  const [img, setImg] = useState();
+
+  const addItem = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("name", name);
+    formData.append("category", category);
+    formData.append("price", price);
+    formData.append("description", description);
+    formData.append("img", img);
+
+    await axios.post(`admin/upload`, formData).then(({ data }) => {
+      Swal.fire({
+        icon: "success",
+        text: data.message,
+      });
+      history.push("/");
+    });
+  };
+
   return (
     <>
       <MainNav />
@@ -16,13 +46,17 @@ const FullMenu = () => {
             <i>Upload Item</i>
           </h1>
           <br />
-          <form>
+          <Form onSubmit={addItem}>
             <div class="form-group">
               <input
                 type="text"
                 class="form-control"
                 placeholder="Enter name..."
                 name="name"
+                value={name}
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
                 aria-describedby="inputGroupFileAddon04"
                 aria-label="Upload"
               />
@@ -34,6 +68,10 @@ const FullMenu = () => {
                 class="form-control"
                 placeholder="Enter category..."
                 name="category"
+                value={category}
+                onChange={(event) => {
+                  setCategory(event.target.value);
+                }}
                 aria-describedby="inputGroupFileAddon04"
                 aria-label="Upload"
               />
@@ -45,6 +83,10 @@ const FullMenu = () => {
                 class="form-control"
                 placeholder="Enter price..."
                 name="price"
+                value={price}
+                onChange={(event) => {
+                  setPrice(event.target.value);
+                }}
                 aria-describedby="inputGroupFileAddon04"
                 aria-label="Upload"
               />
@@ -56,6 +98,10 @@ const FullMenu = () => {
                 class="form-control"
                 placeholder="Enter description..."
                 name="description"
+                value={description}
+                onChange={(event) => {
+                  setDescription(event.target.value);
+                }}
                 aria-describedby="inputGroupFileAddon04"
                 aria-label="Upload"
               />
@@ -66,6 +112,10 @@ const FullMenu = () => {
                 type="file"
                 class="form-control"
                 name="img"
+                value={img}
+                onChange={(event) => {
+                  setImg(event.target.value);
+                }}
                 aria-describedby="inputGroupFileAddon04"
                 aria-label="Upload"
               />
@@ -77,7 +127,7 @@ const FullMenu = () => {
               </button>
             </div>
             <br />
-          </form>
+          </Form>
         </Container>
       </section>
       <Footer />
